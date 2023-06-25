bd1e0094ab8e80b97a74b35d1ee79dd1e4ce326b
Piotr Kubala <piotr.kubala@o2.pl>
2023-06-25 12:03:44 +0200

    Alt pattern added

--- src/main/java/pl/edu/agh/kis/App.java
+++ src/main/java/pl/edu/agh/kis/App.java
@@ -1,7 +1,18 @@
 package pl.edu.agh.kis;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import pl.edu.agh.kis.compiler.Compiler;
+
 public class App {
-    public static void main( String[] args ) {
-        System.out.println("Hello world");
+    public static void main(String[] args) {
+        try {
+            String pattern = new String(System.in.readAllBytes(), StandardCharsets.UTF_8);
+            String code = Compiler.compile(pattern, Compiler.Language.PYTHON);
+            System.out.print(code);
+        } catch (IOException e) {
+            System.err.println("Error while reading stdio");
+        }
     }
 }
